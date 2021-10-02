cc0f3d602bca91f6b9b92b278680d456714b3a42
rocks-tar <73073343+rocks-tar@users.noreply.github.com>
2021-10-02 20:33:35 +0530

    Written the recursive code of knapsack

---
+++ 2.10.2021/Knapsack_recursive.cpp
@@ -0,0 +1,12 @@
+#include <iostream.h>
+using namespace std;
+
+int knapsack(int wt[], int val[], int W, int n) {
+  
+  if(n==0 || W==0)
+    return 0;
+  if(wt[n-1] > W)
+    return knapsack(wt, val, W, n-1);
+  else
+    return max(knapsack(wt, val, W, n-1), knapsack(wt, val, W-wt[n-1], n-1));
+}
